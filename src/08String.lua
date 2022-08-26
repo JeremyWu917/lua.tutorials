0e198fe3becfe83fa868f1bb0503395c0e74e31b
wuzengjie <zengjie.wu@weintdata.com>
2022-08-26 15:11:13 +0800

    Update string sample

---
+++ src/08String.lua
@@ -0,0 +1,12 @@
+--#region
+    --Common string
+--#endregion
+
+local string1 = "This is string one"
+local string2 = 'This is string two'
+local string3 = [[This is string three]]
+
+print(string1)
+print(string2)
+print(string3)
+
